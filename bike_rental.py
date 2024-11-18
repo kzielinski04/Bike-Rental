5b3906696e9cbb80adb0514e9aa31e161365b3b7
Kacper Zieliński <zielinski_kacper@outlook.com>
2024-11-18 10:54:16 +0100

    fix: Add rental duration value validation to calculate_cost function

--- bike_rental.py
+++ bike_rental.py
@@ -5,5 +5,9 @@ def calculate_cost(rental_duration:int)->int:
     match rental_duration:
         case 1:
             return 10
+        case rental_duration if isinstance(rental_duration, int) == False:
+            print("Rental duration must be an integer!\n")
+        case rental_duration if rental_duration <= 0:
+            print("Invalid rental duration!\n")
         case _:
             return 10 + (5 * (rental_duration - 1))
